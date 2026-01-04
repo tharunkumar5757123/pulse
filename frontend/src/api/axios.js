1d5cffd64f849b829e6e1517a0228e1ba2991ffd
tharunkumar5757123 <tharun5757123@gmail.com>
2026-01-04 18:00:24 +0530

    updated public

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  // baseURL: "http://localhost:5000/api",
+  baseURL: "https://pulse-backend-u4cm.onrender.com/api",
   headers: {
     "Content-Type": "application/json", // ensure JSON is sent
   },
